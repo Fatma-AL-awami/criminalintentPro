89a19f0edd3ad62570556730f264cf6539564997
fatma hassan <nihankamal2020@gmail.com>
2020-10-22 07:48:45 +0300

    Efficient RecyclerView Reloading"challenge12"

---
+++ app/src/main/java/com/example/cri/CrimeDetailViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.cri
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class CrimeDetailViewModel: ViewModel() {
+
+    private val crimeRepository = CrimeRepository.get()
+    private val crimeIdLiveData = MutableLiveData<UUID>()
+
+    var crimeLiveData: LiveData<Crime?> =
+        Transformations.switchMap(crimeIdLiveData, crimeRepository::getCrime)
+
+    fun loadCrime(crimeId: UUID) {
+        crimeIdLiveData.value = crimeId
+    }
+
+    fun saveCrime(crime: Crime) {
+        crimeRepository.updateCrime(crime)
+    }
+}
---
+++ app/src/main/java/com/example/cri/CrimeDiffutil.kt
@@ -0,0 +1,16 @@
+package com.example.cri
+
+import androidx.recyclerview.widget.DiffUtil
+
+class CrimeDiffutil: DiffUtil.ItemCallback<Crime>() {
+    override fun areItemsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+        return oldItem.id == newItem.id
+
+    }
+
+    override fun areContentsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+        return oldItem == newItem
+
+    }
+
+}
